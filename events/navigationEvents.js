ff4186dc9b9d0d5aaf7a7188a40c81cf0603e881
frank campos <frankcampos85@gmail.com>
2023-12-06 06:53:21 -0600

    I added to the  click event my vocabularyEntry form

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,10 +1,12 @@
 import { signOut } from '../utils/auth';
+import addVocabularyForm from '../components/forms/addVocabularyEntry';
 
 const navigationEvents = () => {
   document.querySelector('#google-auth').addEventListener('click', signOut);
 
   document.querySelector('#create-entry').addEventListener('click', () => {
     console.warn('CLICKED CREATE ENTRY');
+    addVocabularyForm();
   });
 };
 
