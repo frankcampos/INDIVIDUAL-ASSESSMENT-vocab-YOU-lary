23bd1c427c0cd6936bd0dabbdf56a079c20555dd
frank campos <frankcampos85@gmail.com>
2023-12-09 11:09:14 -0600

    my searchbar is in navbar

--- components/shared/navBAr.js
+++ components/shared/navBAr.js
@@ -5,11 +5,17 @@ const navBar = () => {
   <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-5">
       <div class="container-fluid">
           <a class="navbar-brand" href="#">logo</a>
-          
+
           <!-- Centered button -->
           <div class="mx-auto">
               <a href="#" class="btn btn-primary" id="my-vocabulary">My Vocabulary</a>
           </div>
+
+          <!-- Search Form -->
+          <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
 
           <div class="d-lg-flex justify-content-end">
               <a href="#" class="btn btn-info me-2" id="create-entry">Create New Entry</a>
