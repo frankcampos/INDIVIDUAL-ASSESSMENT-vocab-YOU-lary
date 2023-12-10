fde3acfacbc1037d3e11d4e12845c8d4e838c618
frank campos <frankcampos85@gmail.com>
2023-12-09 20:04:54 -0600

    search bar can look for keywords in your title

--- components/shared/navBAr.js
+++ components/shared/navBAr.js
@@ -2,27 +2,28 @@ import renderToDOM from '../../utils/renderToDom';
 
 const navBar = () => {
   const domString = `
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-5">
-      <div class="container-fluid">
-          <a class="navbar-brand" href="#">logo</a>
+  <<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-5">
+  <div class="container-fluid">
+      <a class="navbar-brand" href="#">logo</a>
 
-          <!-- Centered button -->
-          <div class="mx-auto">
-              <a href="#" class="btn btn-primary" id="my-vocabulary">My Vocabulary</a>
-          </div>
-
-          <!-- Search Form -->
-          <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-
-          <div class="d-lg-flex justify-content-end">
-              <a href="#" class="btn btn-info me-2" id="create-entry">Create New Entry</a>
-              <a href="#" id="logout">Logout</a>
-          </div>
+      <!-- Centered button -->
+      <div class="mx-auto">
+          <a href="#" class="btn btn-primary" id="my-vocabulary">My Vocabulary</a>
       </div>
-  </nav>
+
+      <!-- Search Form with margin right -->
+      <form class="d-flex mr-3" role="search">
+          <input class="form-control me-2" type="search" id='search' placeholder="Search" aria-label="Search">
+         
+      </form>
+
+      <div class="d-lg-flex justify-content-end">
+          <a href="#" class="btn btn-info me-2" id="create-entry">Create New Entry</a>
+          <a href="#" id="logout">Logout</a>
+      </div>
+  </div>
+</nav>
+
 `;
 
   renderToDOM('#navigation', domString);
--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -17,6 +17,29 @@ const navigationEvents = (user) => {
     clearDom();
     getvocabularyCards(user.uid).then(showvocabularyEntry);
   });
+
+  // STRETCH: SEARCH
+  document.querySelector('#search').addEventListener('keyup', (e) => {
+    const searchVocabTitle = (searchValue) => getvocabularyCards(user.uid).then((vocabularyArray) => vocabularyArray.filter((vocabulary) => vocabulary.title.toLowerCase().includes(searchValue.toLowerCase())));
+
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      const searchValue = document.querySelector('#search').value;
+      searchVocabTitle(searchValue)
+        .then((data) => {
+          console.warn(data);
+          console.warn(typeof data);
+          showvocabularyEntry(data);
+        })
+        .then(() => {
+          // Clear the input field after displaying the search results
+          document.querySelector('#search').value = '';
+        })
+        .catch((error) => {
+          console.error('Error:', error); // Added error handling
+        });
+    }
+  });
 };
 
 export default navigationEvents;
